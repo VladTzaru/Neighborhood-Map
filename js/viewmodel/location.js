15efee0b05f5fde74cba17c6d4dde5b07b48a4ae
Vladimir Bojovic <bvanja.87@gmail.com>
2017-11-03 17:54:10 +0100

    Error handler

--- js/viewmodel/location.js
+++ js/viewmodel/location.js
@@ -8,6 +8,7 @@ class Location {
         this.phone;
         this.category;
         this.fullAddress;
+        this.url;
 
         // This is our Foursquare API url
         const foursquareURL = `https://api.foursquare.com/v2/venues/search?v=20161016&ll=${this.geoLoc.lat}%2C%20${this.geoLoc.lng}&query=${this.title}&intent=checkin&client_id=NMNSC2SLOPCO3DYBPGGQSIP3LOYYMZMG2GSBDJ0XFDFZFNKH&client_secret=J12J4HIQYKOJ5M0H2EXIIFJMEWUQ5JANO4IRG15QIFPWEREL`;
@@ -54,7 +55,8 @@ class Location {
                 <h3 class="infowindow-title">${self.title}</h3>
                 <p class="infowindow-category">Phone: <span class="infowindow-content">${self.phone}</span></p>
                 <p class="infowindow-category">Category: <span class="infowindow-content">${self.category}</span></p>
-                <p class="infowindow-category">Address: <span class="infowindow-content">${self.fullAddress}</span></p>`;
+                <p class="infowindow-category">Address: <span class="infowindow-content">${self.fullAddress}</span></p>
+                <p class="infowindow-category">Website: <span class="infowindow-content">${self.url}</span></p>`;
 
             // Check to make sure the infowindow is not already opened on this marker.
             if (infowindow.marker != marker) {
@@ -90,6 +92,26 @@ class Location {
                         self.phone = response.contact.formattedPhone;
                         self.category = response.categories[0].name;
                         self.fullAddress = response.location.formattedAddress;
+                        self.url = response.url;
+
+                        // Handle cases when API data is unavailable
+                        if (self.phone === undefined || self.phone === null || self.phone === '') {
+                            self.phone = 'Not provided';
+                        }
+
+                        if (self.category === undefined || self.category === null || self.category === '') {
+                            self.category = 'Not provided';
+                        }
+
+                        if (self.fullAddress === undefined || self.fullAddress === null || self.fullAddress === '') {
+                            self.fullAddress = 'Not provided';
+                        }
+
+                        if (self.url === undefined || self.url === null || self.url === '') {
+                            self.url = 'Not provided';
+                        } else {
+                            self.url = `<a href="${self.url}" target="_blank">${self.url}</a>`;
+                        }
 
                         // Populate infowindow with updated information
                         self.populateInfoWindow(this, infoWindow);

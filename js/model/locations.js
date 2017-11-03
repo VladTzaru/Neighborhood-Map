5b4731eeae319f4f4fc74e8912035fc0a2087dd0
Vladimir Bojovic <bvanja.87@gmail.com>
2017-11-03 02:51:14 +0100

    Add more locations

--- js/model/locations.js
+++ js/model/locations.js
@@ -3,8 +3,9 @@ const defaultLocations = [
     { title: 'Astal Saren', location: {lat: 45.256798, lng: 19.845963} },
     { title: 'Pizzeria Savoca', location: {lat: 45.260121, lng: 19.832831} },
     { title: 'EmChi', location: {lat: 45.257744, lng: 19.843152} },
-    { title: 'Zak', location: {lat: 45.254331, lng: 19.841092} },
     { title: 'Veliki Cafe', location: {lat: 45.258454, lng: 19.846167} },
     { title: 'Gondola', location: {lat: 45.253772, lng: 19.850780} },
-    { title: 'Voyager b&b', location: {lat: 45.248382, lng: 19.848906} }
+    { title: 'Voyager b&b', location: {lat: 45.248382, lng: 19.848906} },
+    { title: 'Hotel Centar', location: {lat: 45.254537, lng: 19.841857} },
+    { title: 'Wake Up', location: {lat: 45.251259, lng: 19.845634} }
 ];

4d5153fa144e278c8a6693aa6540cb4c8edb1bc4
Vladimir Bojovic <bvanja.87@gmail.com>
2017-11-03 19:30:29 +0100

    Add more locations

--- js/model/locations.js
+++ js/model/locations.js
@@ -1,6 +1,7 @@
 // Default locations
 const defaultLocations = [
     { title: 'Astal Saren', location: {lat: 45.256798, lng: 19.845963} },
+    { title: 'Petrovaradin Fortress', location: {lat: 45.252991, lng: 19.863472} },
     { title: 'Pizzeria Savoca', location: {lat: 45.260121, lng: 19.832831} },
     { title: 'EmChi', location: {lat: 45.257744, lng: 19.843152} },
     { title: 'Galerija Matice Srpske', location: {lat: 45.252644, lng: 19.845696} },
@@ -9,5 +10,8 @@ const defaultLocations = [
     { title: 'Jevrejska Sinagoga', location: {lat: 45.253086, lng: 19.840745} },
     { title: 'Voyager b&b', location: {lat: 45.248382, lng: 19.848906} },
     { title: 'Hotel Centar', location: {lat: 45.254537, lng: 19.841857} },
-    { title: 'Wake Up', location: {lat: 45.251259, lng: 19.845634} }
+    { title: 'Lanterna', location: {lat: 45.256424, lng: 19.850034} },
+    { title: 'Wake Up', location: {lat: 45.251259, lng: 19.845634} },
+    { title: 'Akva Doria', location: {lat: 45.254075, lng: 19.859543} },
+    { title: 'Biblioteka', location: {lat: 45.263982, lng: 19.848007} }
 ];
